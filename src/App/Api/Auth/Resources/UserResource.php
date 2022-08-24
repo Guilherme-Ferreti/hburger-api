74506c65d7bb99fdf5b5450bcb1b43cffa16fe7d
Guilherme-Ferreti <guiferretis@gmail.com>
2022-08-24 20:46:17 -0300

    Changed responses to camel case

--- src/App/Api/Auth/Resources/UserResource.php
+++ src/App/Api/Auth/Resources/UserResource.php
@@ -9,11 +9,11 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'         => $this->id,
-            'name'       => $this->name,
-            'email'      => $this->email,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'id'        => $this->id,
+            'name'      => $this->name,
+            'email'     => $this->email,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
         ];
     }
 }
--- src/App/Api/Products/Resources/BreadResource.php
+++ src/App/Api/Products/Resources/BreadResource.php
@@ -9,11 +9,11 @@ class BreadResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'         => $this->id,
-            'name'       => $this->name,
-            'price'      => $this->price,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'id'        => $this->id,
+            'name'      => $this->name,
+            'price'     => $this->price,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
         ];
     }
 }
